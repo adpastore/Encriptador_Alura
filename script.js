0c1342d72165448651e00aab009c59169f66db98
Andres Pastore <adpastore@gmail.com>
2023-05-26 21:03:12 -0300

    mejora de explicaciones

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function desencriptar(stringDesencriptada) {
 }
 
 function btnCopiar() {
-  let texto = document.getElementById("mensaje").value;               //asigna a texto el valor "Ej.:hola"
+  let texto = document.getElementById("mensaje").value;               //asigna a texto el valor "hola"
   let textareaIngresar = document.querySelector(".textarea.ingreso"); //identifica el destino y le asigna una variable
   textareaIngresar.value = texto;                                     // A la variable de destino le asigna el "hola"
   navigator.clipboard.writeText(texto).then(function() {});           //Copia en el portapapeles el "hola"
